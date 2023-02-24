ac4efb68e2414e6eedc2e5796b64ba8ce6a92e82
João Antonio - Ikidon Dev <ikidon.dev@gmail.com>
2023-02-24 19:44:27 -0300

    added mock messages

---
+++ web/src/mock/messages.ts
@@ -0,0 +1,22 @@
+export const messagesMock = [
+  {
+    name: "Kira",
+    body: "Tive uma ideia incrível para um projeto! 😍",
+    createdAt: "Fri Feb 24 2023 11:30:48 GMT-0300 (Brasilia Standard Time)",
+  },
+  {
+    name: "Ikidon",
+    body: "Sério? Me conta mais.",
+    createdAt: "Fri Feb 24 2023 11:32:48 GMT-0300 (Brasilia Standard Time)",
+  },
+  {
+    name: "Kira",
+    body: "E se a gente fizesse um chat moderno e responsivo em apenas uma semana?",
+    createdAt: "Fri Feb 24 2023 11:34:48 GMT-0300 (Brasilia Standard Time)",
+  },
+  {
+    name: "Ikidon",
+    body: "#boraCodar! 🚀",
+    createdAt: "Fri Feb 24 2023 11:36:48 GMT-0300 (Brasilia Standard Time)",
+  },
+];
